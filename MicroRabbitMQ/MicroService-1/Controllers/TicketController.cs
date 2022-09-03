9157989d6b712e1317c4a1dadcc9087e32fe5f9c
Yogesh <yogeshbietjhs@gmail.com>
2022-09-03 16:17:07 +0530

    MicroServices-1 and MicroService consumer

---
+++ MicroRabbitMQ/MicroService-1/Controllers/TicketController.cs
@@ -0,0 +1,33 @@
+﻿using MicroService_1.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+using MassTransit;
+
+namespace MicroService_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly IBus _bus;
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            if (ticket != null)
+            {
+                ticket.BookedOn = DateTime.Now;
+                Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                var endPoint = await _bus.GetSendEndpoint(uri);
+                await endPoint.Send(ticket);
+                return Ok("Success");
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ MicroRabbitMQ/RabbitMQ.producer/Program.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQ.producer
+{
+    public class Program
+    {
+        static  void Main(string[] args)
+        {
+            var Factory = new ConnectionFactory { 
+                Uri}
+        }
+    }
+}
